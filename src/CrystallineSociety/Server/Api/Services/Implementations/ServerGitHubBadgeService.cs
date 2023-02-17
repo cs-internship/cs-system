2d331f7cbc6da85fd5c72a459d798dce2872a9cb
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-02-17 19:57:43 +0330

    Add SeverGitHubBadgeService

--- src/CrystallineSociety/Server/Api/Services/Implementations/ServerGitHubBadgeService.cs
+++ src/CrystallineSociety/Server/Api/Services/Implementations/ServerGitHubBadgeService.cs
@@ -1,4 +1,5 @@
 ﻿using CrystallineSociety.Shared.Dtos.BadgeSystem;
+using Octokit;
 
 namespace CrystallineSociety.Server.Api.Services.Implementations
 {
@@ -9,9 +10,22 @@ namespace CrystallineSociety.Server.Api.Services.Implementations
             throw new NotImplementedException();
         }
 
-        public Task<BadgeDto> GetBadgeAsync(string url)
+        public async Task<BadgeDto> GetBadgeAsync(string url)
         {
             throw new NotImplementedException();
+            //var client = new GitHubClient(new ProductHeaderValue("CS-System"));
+            //var repos =  await client.Repository.GetAllForOrg("cs-internship");
+            //var repo = repos.First(r => r.Name == "cs-system");
+
+            //var refs= await client.Git.Reference.GetAll(repo.Id);
+
+            //var main = refs.First(r => r.Ref.Contains("refs/heads/main"));
+            ////var main = refs.First(r => r.Ref.Contains("refs/heads/main"));
+            //var refx =
+            //    "refs/heads/main/src/CrystallineSociety/Shared/CrystallineSociety.Shared.Test/BadgeSystem/SampleBadgeDocs/serialization-badge-sample";
+            //var xx = await client.Git.Tree.GetRecursive(repo.Id, main.Ref);
+
+            //return default;
         }
     }
 }
