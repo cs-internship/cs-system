d8636aa29eb278a238f11b837e0617b787675ed9
Zahra Ahangari <zahra_ahangari@outlook.com>
2023-03-29 20:32:36 +0330

    Add FileContentIsNullException type

---
+++ src/CrystallineSociety/Shared/Shared/Exceptions/FileContentIsNullException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace CrystallineSociety.Shared.Exceptions;
+
+public class FileContentIsNullException : IOException
+{
+    public FileContentIsNullException()
+    : base(nameof(AppStrings.FileContentIsNullException))
+    {
+    }
+
+    public FileContentIsNullException(string message)
+        : base(message)
+    {
+    }
+
+    public FileContentIsNullException(string message, Exception? innerException)
+        : base(message, innerException)
+    {
+    }
+}
