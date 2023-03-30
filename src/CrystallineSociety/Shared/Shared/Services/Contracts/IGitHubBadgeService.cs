a0ce4e835e46d86b35b271cc590fe9079ba2d0c1
BSeyfi <42473071+BSeyfi@users.noreply.github.com>
2023-03-30 12:34:14 +0330

    Improve GetBadgeAsync documentation

--- src/CrystallineSociety/Shared/Shared/Services/Contracts/IGitHubBadgeService.cs
+++ src/CrystallineSociety/Shared/Shared/Services/Contracts/IGitHubBadgeService.cs
@@ -19,10 +19,12 @@ namespace CrystallineSociety.Shared.Services.Contracts
         /// <summary>
         /// Loads a badge spec from the given <paramref name="url"/> and parses it, and returns a BadgeDto.
         /// </summary>
-        /// <param name="url"></param>
+        /// <param name="url">The url address that contains badge files or folders containing badge file/folders.</param>
         /// <returns>Returns the parsed badge.</returns>
         /// <exception cref="FormatException">If the loaded url string is not in a correct format.</exception>
         /// <exception cref="ResourceNotFoundException">If the given url could not be found.</exception>
+        /// <exception cref="FileNotFoundException">If the given url contains no valid badge file.</exception>
+        /// <exception cref="FileContentIsNullException">If the badge file content is null.</exception>
         Task<BadgeDto> GetBadgeAsync(string url);
     }
 }
