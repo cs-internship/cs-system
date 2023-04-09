1021893b82dc821fc4d7cb8a88f7ec0dab43be39
Hootan HT <hootanhemmati@outlook.com>
2023-04-09 14:43:41 +0330

    Add BadgeTree Component.

---
+++ src/CrystallineSociety/Client/Shared/Components/BadgeTree.razor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CrystallineSociety.Shared.Dtos.BadgeSystem;
+
+namespace CrystallineSociety.Client.Shared.Components
+{
+    public partial class BadgeTree
+    {
+        [Parameter] public BadgeBundleDto? BadgeBundleDto { get; set; }
+
+        private string?[]? BadgeCodes { get; set; }
+
+        protected override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (BadgeBundleDto != null && firstRender)
+            {
+                BadgeCodes = BadgeBundleDto.Badges.Select(b => b.Code).ToArray();
+            }
+            return base.OnAfterRenderAsync(firstRender);
+        }
+
+        private void Test()
+        {
+            if (BadgeBundleDto != null)
+            {
+                BadgeCodes = BadgeBundleDto.Badges.Select(b => b.Code).ToArray();
+            }
+        }
+    }
+}
