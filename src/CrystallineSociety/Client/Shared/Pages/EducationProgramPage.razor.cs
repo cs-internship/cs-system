4ea858e0e2ad218669ded213f50d49a7fa89fa7c
Hootan Hemmati <hootanhemmati@outlook.com>
2023-10-25 22:07:21 +0330

    Complete education sync page. #16687

--- src/CrystallineSociety/Client/Shared/Pages/EducationProgramPage.razor.cs
+++ src/CrystallineSociety/Client/Shared/Pages/EducationProgramPage.razor.cs
@@ -9,14 +9,21 @@ using CrystallineSociety.Shared.Dtos.EducationProgram;
 namespace CrystallineSociety.Client.Shared.Pages;
 public partial class EducationProgramPage
 {
-    private List<EducationProgramDto> educationPrograms = new();
+    private List<EducationProgramDto> EducationPrograms = new();
+    private bool IsSyncing = false;
 
 
     protected override async Task OnInitAsync()
     {
         // Todo : complete this code to return dto not entity.
-        educationPrograms = await HttpClient.GetFromJsonAsync<List<EducationProgramDto>>("BadgeSystem/GetEducationPrograms");
-        await HttpClient.PostAsJsonAsync("BadgeSystem/SyncEducationProgramBadges", educationPrograms);
+        EducationPrograms = await HttpClient.GetFromJsonAsync<List<EducationProgramDto>>("BadgeSystem/GetEducationPrograms");
         await base.OnInitAsync();
+    }
+
+    private async Task HandelSyncAsync(EducationProgramDto educationProgram)
+    {
+        IsSyncing = true;
+        await HttpClient.PostAsJsonAsync("BadgeSystem/SyncEducationProgramBadges", educationProgram);
+        IsSyncing = false;
     }
 }
