4f1eb3c874212eb4a45fa6c6564c62deb324b445
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-11-05 18:15:15 +0330

    Structure the first layout

---
+++ src/CrystallineSociety/Client/Shared/Components/OrganizationBadges.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystallineSociety.Client.Shared.Components
+{
+    public partial class OrganizationBadges
+    {
+        [Parameter]
+        public OrganizationDto Organization { get; set; }
+    }
+}
---
+++ src/CrystallineSociety/Client/Shared/Pages/OrganizationPage.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystallineSociety.Client.Shared.Pages
+{
+    public partial class OrganizationPage
+    {
+        [Parameter]
+        public string OrganizationCode { get; set; }
+
+        private OrganizationDto Organization { get; set; }
+
+        protected override async Task OnInitAsync()
+        {
+            await LoadOrganizationAsync();
+            await base.OnInitAsync();
+        }
+
+        private async Task LoadOrganizationAsync()
+        {
+            Organization = new OrganizationDto(){Code = "CSI", Title = "CS Internship", Description = "Our first program"};
+        }
+    }
+}
