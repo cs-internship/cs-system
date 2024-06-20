25befeb892244f08da476d7bfa349212baf3b83d
Ali Sadeghi <ali909392@gmail.com>
2024-06-20 22:15:07 +0330

    Improve BadgeTree code structure / Use svg for arrows / Backup before using accordion

--- src/CrystallineSociety/Client/CrystallineSociety.Client.Core/Components/BadgeTree.razor.cs
+++ src/CrystallineSociety/Client/CrystallineSociety.Client.Core/Components/BadgeTree.razor.cs
@@ -2,20 +2,22 @@
 
 namespace CrystallineSociety.Client.Core.Components
 {
-    public partial class BadgeTree
+    public partial class BadgeTree : ComponentBase
     {
         [Parameter] public BadgeBundleDto? BadgeBundleDto { get; set; }
         [Parameter] public EventCallback<BadgeDto> BadgeDtoCallBack { get; set; }
 
         private string? ActiveBadgeUrl { get; set; }
-
         private List<BadgeDto>? Badges { get; set; }
         private Dictionary<int, bool> accordionCollapsed = new Dictionary<int, bool>
         {
+            { 0, true },
             { 1, true },
             { 2, true },
             { 3, true }
         };
+
+        private static readonly BadgeLevel[] BadgeLevels = { BadgeLevel.Gold, BadgeLevel.Silver, BadgeLevel.Bronze };
 
         private void ToggleCollapseAccordion(int itemNumber)
         {
@@ -27,13 +29,12 @@ namespace CrystallineSociety.Client.Core.Components
             return !accordionCollapsed[itemNumber] ? "show" : "";
         }
 
-        protected override Task OnParamsSetAsync()
+        protected override void OnParametersSet()
         {
             if (BadgeBundleDto != null)
             {
                 Badges = BadgeBundleDto.Badges.ToList();
             }
-            return base.OnParamsSetAsync();
         }
 
         private async Task OnBadgeClick(BadgeDto badgeDto)
