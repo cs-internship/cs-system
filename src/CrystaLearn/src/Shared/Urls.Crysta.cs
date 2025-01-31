01e94dec6b46f05dd39f39affbed23d338514118
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-01-31 13:44:33 +0330

    Add urls

---
+++ src/CrystaLearn/src/Shared/Urls.Crysta.cs
@@ -0,0 +1,17 @@
+﻿namespace CrystaLearn.Shared;
+
+public static partial class Urls
+{
+    public static class Crysta
+    {
+        public const string DocsPageRoutePattern = "/{ProgramCode}/docs";
+        public static ProgramPage Program(string program) => new(program);
+
+        public class ProgramPage(string programCode)
+        {
+            public string DocsPage = $"/{programCode}/docs";
+        }
+    }
+
+    
+}
