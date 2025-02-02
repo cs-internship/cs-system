5ff339f2bb4000f5147f951ea3742e79867be1bf
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-02-03 00:13:05 +0330

    Add core.test project.

---
+++ src/CrystaLearn/src/Core/CrystaLearn.Core.Tests/Infra/TestBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrystaLearn.Core.Extensions;
+using Microsoft.Extensions.Hosting;
+
+namespace CrystaLearn.Core.Tests.Infra;
+public class TestBase
+{
+    protected IServiceProvider CreateServiceProvider(Action<IServiceCollection>? configServices = null)
+    {
+        var builder = new HostApplicationBuilder();
+        builder.AddCrystaServices();
+
+        if (configServices is not null)
+        {
+            configServices(builder.Services);
+        }
+
+        var host = builder.Build();
+
+        return host.Services;
+    }
+}
