680b0fd344e6bc4483087f74eedeb440c0d9b443
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-02-06 14:36:12 +0330

    Add AzureBoardService interfaces.

---
+++ src/CrystaLearn/src/Core/CrystaLearn.Core/Services/AzureBoard/AzureBoardService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrystaLearn.Core.Services.Contracts;
+
+namespace CrystaLearn.Core.Services.AzureBoard;
+public class AzureBoardService : IAzureBoardService
+{
+}
