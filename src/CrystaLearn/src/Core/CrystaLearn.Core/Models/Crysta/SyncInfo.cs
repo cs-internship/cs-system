3680eeb12bdbf62d26bcf7e65ce3f27bab7a664a
Mehran Davoudi <mehran.davoudi@hotmail.com>
2025-04-19 01:20:20 +0330

    Fix SyncInfo nullables.

--- src/CrystaLearn/src/Core/CrystaLearn.Core/Models/Crysta/SyncInfo.cs
+++ src/CrystaLearn/src/Core/CrystaLearn.Core/Models/Crysta/SyncInfo.cs
@@ -4,11 +4,11 @@
 public class SyncInfo
 {
     [MaxLength(100)]
-    public string SyncId { get; set; }
-    public DateTimeOffset SyncStartDateTime { get; set; }
-    public DateTimeOffset SyncEndDateTime { get; set; }
+    public string? SyncId { get; set; }
+    public DateTimeOffset? SyncStartDateTime { get; set; }
+    public DateTimeOffset? SyncEndDateTime { get; set; }
     [MaxLength(100)]
-    public string SyncHash { get; set; } = string.Empty;
+    public string? SyncHash { get; set; } = string.Empty;
     public SyncStatus? SyncStatus { get; set; }
     public DateTimeOffset? LastSyncDateTime { get; set; }
     [MaxLength(40)]
--- src/CrystaLearn/src/Shared/Dtos/Crysta/SyncInfoDto.cs
+++ src/CrystaLearn/src/Shared/Dtos/Crysta/SyncInfoDto.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 namespace CrystaLearn.Shared.Dtos.Crysta;
 public class SyncInfoDto
 {
-    public Guid SyncId { get; set; }
-    public DateTimeOffset SyncStartDateTime { get; set; }
-    public DateTimeOffset SyncEndDateTime { get; set; }
+    public Guid? SyncId { get; set; }
+    public DateTimeOffset? SyncStartDateTime { get; set; }
+    public DateTimeOffset? SyncEndDateTime { get; set; }
     [MaxLength(100)]
     public string? SyncHash { get; set; }
     public SyncStatus? SyncStatus { get; set; }
